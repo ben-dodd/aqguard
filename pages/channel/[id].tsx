8fa0a85920fcbc087f98f924473d803c2663b01c
Ben D <thebillham@gmail.com>
2021-08-23 22:47:05 +1200

    Fix

--- pages/channel/[id].tsx
+++ pages/channel/[id].tsx
@@ -1,33 +1,26 @@
-import { useRouter } from "next/router";
+// import { useRouter } from "next/router";
 
 // import { useEntry } from '@/lib/swr-hooks'
 import Container from "@/components/container";
-import Nav from "@/components/nav";
 
 export default function ChannelPage() {
-  const router = useRouter();
-  const id = router.query.id?.toString();
+  // const router = useRouter();
+  // const id = router.query.id?.toString();
   // const { data } = useEntry(id)
-
+  const data = { title: "Test", content: "Test content" };
   if (data) {
     return (
-      <>
-        <Nav title="View" />
-        <Container>
-          <h1 className="font-bold text-3xl my-2">{data.title}</h1>
-          <p>{data.content}</p>
-        </Container>
-      </>
+      <Container>
+        <h1 className="font-bold text-3xl my-2">{data.title}</h1>
+        <p>{data.content}</p>
+      </Container>
     );
   } else {
     return (
-      <>
-        <Nav title="View" />
-        <Container>
-          <h1 className="font-bold text-3xl my-2">...</h1>
-          <p>...</p>
-        </Container>
-      </>
+      <Container>
+        <h1 className="font-bold text-3xl my-2">...</h1>
+        <p>...</p>
+      </Container>
     );
   }
 }
